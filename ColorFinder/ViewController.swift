d32338b0c9dbef2ecd2fc54121ada61f00e79b84
Derick Peterson <dj9067p@gmail.com>
2018-12-05 21:53:44 -0600

    Initial Commit

---
+++ ColorFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorFinder
+//
+//  Created by Derick Peterson on 12/5/18.
+//  Copyright © 2018 Derick Peterson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
