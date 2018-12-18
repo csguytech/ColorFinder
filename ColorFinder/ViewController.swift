64554c70ad1eb17a9c5de4e13a6e298628693595
Derick Peterson <dj9067p@gmail.com>
2018-12-18 12:25:40 -0600

    Version 1 completed ColorFinder

--- ColorFinder/ViewController.swift
+++ ColorFinder/ViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import PhotosUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var hexValue: UITextField!
+    @IBOutlet weak var rgbValue: UITextField!
+    var image: UIImage!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+    @IBAction func loadImage(_ sender: Any) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let tapPoint = touch.location(in: self.view)
+            let loc = tapPoint
+            let color = imageView.colorOfTouch(point: loc)
 
+            rgbValue.text = color.rgb()
+            hexValue.text = color.hex()
+            colorView.backgroundColor = color
+        }
+    }
+    
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            fatalError("No image, \(info)")
+        }
 
+        imageView.image = selectedImage
+        image = selectedImage
+
+        dismiss(animated: true, completion: nil)
+    }
 }
 
